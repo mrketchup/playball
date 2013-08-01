adfc9e562ff247392f0b346f6ffb656931ed2361
mrketchup <matthewjones1988@gmail.com>
2013-08-01 17:27:18 -0400

    fixed a 9th inning bug

--- src/Game.py
+++ src/Game.py
@@ -54,130 +54,79 @@ class Diamond(object):
         runs = 0
         
         if result == 'OUT':
-            print batter.fullName, 'is out.'
+            pass
             
-        elif result == '1B':
-            print batter.fullName, 'singles.'
-            
+        elif result == '1B':            
             rand = random()
             if self.third is not None:
                 runs += 1
-                print ' *', self.third.fullName, 'scores.'
                 self.third = None
             if self.second is not None:
                 if rand < 0.5:
                     runs += 1
-                    print ' *', self.second.fullName, 'scores.'
                 else:
                     self.third = self.second
-                    print ' *', self.second.fullName, 'goes to third.'
                 self.second = None
             if self.first is not None:
                 if rand < 0.2:
                     self.third = self.first
-                    print ' *', self.first.fullName, 'goes to third.'
                 else:
                     self.second = self.first
-                    print ' *', self.first.fullName, 'goes to second.'
                 self.first = None
             
             self.first = batter
             
-        elif result == '2B':
-            print batter.fullName, 'doubles.'
-            
+        elif result == '2B':            
             if self.third is not None:
                 runs += 1
-                print ' *', self.third.fullName, 'scores.'
                 self.third = None
             if self.second is not None:
                 runs += 1
-                print ' *', self.second.fullName, 'scores.'
                 self.second = None
             if self.first is not None:
                 if random() < 0.2:
                     runs += 1
-                    print ' *', self.first.fullName, 'scores.'
                 else:
                     self.third = self.first
-                    print ' *', self.first.fullName, 'goes to third.'
                 self.first = None
                 
             self.second = batter
             
-        elif result == '3B':
-            print batter.fullName, 'triples.'
-            
+        elif result == '3B':            
             if self.third is not None:
                 runs += 1
-                print ' *', self.third.fullName, 'scores.'
                 self.third = None
             if self.second is not None:
                 runs += 1
-                print ' *', self.second.fullName, 'scores.'
                 self.second = None
             if self.first is not None:
                 runs += 1
-                print ' *', self.first.fullName, 'scores.'
                 self.first = None
                 
             self.third = batter
             
         elif result == 'HR':
-            print batter.fullName, 'homers.'
             runs += 1
             
             if self.third is not None:
                 runs += 1
-                print ' *', self.third.fullName, 'scores.'
                 self.third = None
             if self.second is not None:
                 runs += 1
-                print ' *', self.second.fullName, 'scores.'
                 self.second = None
             if self.first is not None:
                 runs += 1
-                print ' *', self.first.fullName, 'scores.'
                 self.first = None
                 
-        elif result == 'BB':
-            print batter.fullName, 'walks.'
-            
+        elif result == 'BB' or result == 'HBP':            
             if self.third is not None and self.second is not None and self.first is not None:
                 runs += 1
-                print ' *', self.third.fullName, 'scores.'
-                print ' *', self.second.fullName, 'goes to third.'
-                print ' *', self.first.fullName, 'goes to second.'
                 self.third = self.second
                 self.second = self.first
             elif self.second is not None and self.first is not None:
-                print ' *', self.second.fullName, 'goes to third.'
-                print ' *', self.first.fullName, 'goes to second.'
                 self.third = self.second
                 self.second = self.first
             elif self.first is not None:
-                print ' *', self.first.fullName, 'goes to second.'
-                self.second = self.first
-                
-            self.first = batter
-            
-        elif result == 'HBP':
-            print batter.fullName, 'is hit by pitch.'
-            
-            if self.third is not None and self.second is not None and self.first is not None:
-                runs += 1
-                print ' *', self.third.fullName, 'scores.'
-                print ' *', self.second.fullName, 'goes to third.'
-                print ' *', self.first.fullName, 'goes to second.'
-                self.third = self.second
-                self.second = self.first
-            elif self.second is not None and self.first is not None:
-                print ' *', self.second.fullName, 'goes to third.'
-                print ' *', self.first.fullName, 'goes to second.'
-                self.third = self.second
-                self.second = self.first
-            elif self.first is not None:
-                print ' *', self.first.fullName, 'goes to second.'
                 self.second = self.first
                 
             self.first = batter
@@ -203,16 +152,19 @@ class Game(object):
         self.awayTeam = awayTeam if awayTeam is not None else Team()
         self.homeRuns = 0
         self.awayRuns = 0
+        self._isPlayed = False
         
     def play(self):
+        if self._isPlayed:
+            return
+        
+        self._isPlayed = True
         diamond = Diamond()
         inning = 1
         homeSpot = 0 # lineup spot
         awaySpot = 0
         
-        while inning <= 9 or self.homeRuns == self.awayRuns:
-            print '----  TOP  ', str(inning), ' ----'
-            
+        while inning <= 9 or self.homeRuns == self.awayRuns:            
             outs = 0
             while outs < 3:
                 batter = self.awayTeam.lineup[awaySpot]
@@ -221,24 +173,24 @@ class Game(object):
                 self.awayRuns += runs
                 if result == 'OUT':
                     outs += 1
-                awaySpot = awaySpot + 1 if awaySpot < 8 else 0
+                awaySpot = (awaySpot + 1) % 9
                 
             diamond.clear()
             
             if self.homeRuns <= self.awayRuns or inning < 9:
-                print '---- BOTTOM', str(inning), ' ----'
                 
                 outs = 0
                 while outs < 3:
                     if self.homeRuns > self.awayRuns and inning >= 9:
                         break
-                    batter = self.homeTeam.lineup[awaySpot]
+                    batter = self.homeTeam.lineup[homeSpot]
                     result = simulatePA(batter)
                     runs = diamond.advanceRunners(batter, result)
                     self.homeRuns += runs
                     if result == 'OUT':
                         outs += 1
-                    homeSpot = homeSpot + 1 if homeSpot < 8 else 0
+                    homeSpot = (homeSpot + 1) % 9
                     
                 diamond.clear()
-                inning += 1
+            
+            inning += 1
--- src/main.py
+++ src/main.py
@@ -8,40 +8,32 @@ if __name__ == '__main__':
     from Player import Player
     from Team import Team
     from Game import Game
+    from time import time
     
-    joe = Player('Regular', 'Joe')
-    joe.rate1B = 0.150
-    joe.rate2B = 0.050
-    joe.rate3B = 0.005
-    joe.rateHR = 0.050
-    joe.rateBB = 0.100
-    joe.rateHBP = 0.010
+    p = Player()
+    p.rate1B = 0.150
+    p.rate2B = 0.050
+    p.rate3B = 0.005
+    p.rateHR = 0.050
+    p.rateBB = 0.100
+    p.rateHBP = 0.010
     
-    home = Team('Hometown', 'Heroes')
-    home.lineup[0] = joe
-    home.lineup[1] = joe
-    home.lineup[2] = joe
-    home.lineup[3] = joe
-    home.lineup[4] = joe
-    home.lineup[5] = joe
-    home.lineup[6] = joe
-    home.lineup[7] = joe
-    home.lineup[8] = joe
+    home = Team()
+    away = Team()
     
-    away = Team('Outtatown', 'Baddies')
-    away.lineup[0] = joe
-    away.lineup[1] = joe
-    away.lineup[2] = joe
-    away.lineup[3] = joe
-    away.lineup[4] = joe
-    away.lineup[5] = joe
-    away.lineup[6] = joe
-    away.lineup[7] = joe
-    away.lineup[8] = joe
+    for i in range(9):
+        home.lineup[i] = p
+        away.lineup[i] = p
     
-    game = Game(home, away)
-    game.play()
+    GAMES = 10000
     
-    print 'Game Over!'
-    print home.fullName + ':' + str(game.homeRuns)
-    print away.fullName + ':' + str(game.awayRuns)
+    start = time()
+    
+    for i in range(GAMES):
+        game = Game(home, away)
+        game.play()
+        
+    end = time()
+    
+    print 'Time Elapsed:', (end - start)
+    print 'Games/Second:', GAMES / (end - start)
